1ded99a3bcec4dd94a30f05cc185bc34d489f416
Cristian Jaramillo <cristian.jaramillo@bringmeister.de>
2024-01-22 20:21:33 +0100

    Refactor UserController

--- src/main/kotlin/org/jaramo/vissage/adapter/api/UserController.kt
+++ src/main/kotlin/org/jaramo/vissage/adapter/api/UserController.kt
@@ -1,17 +1,16 @@
 package org.jaramo.vissage.adapter.api
 
 import jakarta.validation.Valid
-import jakarta.validation.constraints.NotBlank
+import org.jaramo.vissage.adapter.api.dto.CreateUserRequestDto
+import org.jaramo.vissage.adapter.api.dto.toDto
 import org.jaramo.vissage.application.UserService
-import org.jaramo.vissage.domain.model.Nickname
 import org.jaramo.vissage.infrastructure.toResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
-import java.util.UUID
 
 
 @RestController
@@ -24,16 +23,11 @@ class UserController(
         userService
             .register(requestDto.nickname)
             .map { user ->
-                ResponseEntity.created(
-                    ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.id).toUri()
-                ).body(UserDto(user.id, user.nickname))
+                ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(user.toDto())
             }.getOrElse { error ->
                 error.toResponse()
             }
 }
 
-data class CreateUserRequestDto(
-    @get:NotBlank val nickname: String,
-)
-
-data class UserDto(val id: UUID, val nickname: Nickname)
